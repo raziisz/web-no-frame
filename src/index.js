a2fca62d0bab26d4244c883f7936b5eddc72de84
Luiz Felipe <raziel_libertino@hotmail.com>
2020-11-28 21:14:50 -0400

    post no framework

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 const http = require('http');
 const PORT = 3000;
 const DEFAULT_HEADER = { 'Content-Type': 'application/json'}
+const heroFactory = require('./factories/heroFactory');
+const heroService = heroFactory.generateInstance();
+const Hero = require('./entities/hero');
 
 const routes = {
   '/heroes:get': async(request, response) => {
     const { id } = request.queryString;
-    console.log({ id })
+    const heroes = await heroService.find(id);
+    
+    response.write(JSON.stringify({ results: heroes }))
+   
     return response.end()
+  },
+  '/heroes:post': async (request, response) => {
+    // async interator
+    for await (const data of request) {
+      const item = JSON.parse(data);
+      const hero = new Hero(item);
+      
+      const { error, valid } = hero.isValid();
+      if (!valid) {
+        response.writeHead(400, DEFAULT_HEADER);
+        response.write(JSON.stringify({ error: error.join(', ')}));
+        return response.end();
+      }
+
+      const id = await heroService.create(hero);
+      response.writeHead(201, DEFAULT_HEADER)
+      response.write(JSON.stringify({ success: 'User Created with success!!', id}));
+
+      return response.end();
+    }
   },
   default: (request, response) => {
     response.write('Hello again!');
@@ -20,7 +46,9 @@ const handler = (request, response) => {
   const key = `/${route}:${method.toLowerCase()}`;
   
   response.writeHead(200, DEFAULT_HEADER);
-  response.end()
+
+  const choosen = routes[key] || routes.default;
+  return choosen(request, response);
 }
 
 http.createServer(handler)
